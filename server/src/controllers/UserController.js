6cc2803f10b42a5ac6755f67574b95dcc238bb1d
leonov-o <oleksandr.leonov.work@gmail.com>
2024-05-06 16:58:41 +0300

    Fix minor.

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -60,7 +60,7 @@ class UserController {
         try {
             const {refreshToken} = req.cookies;
             const userData = await userService.refresh(refreshToken);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refresh_token, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             return res.json(userData);
         } catch (e) {
             next(e);
